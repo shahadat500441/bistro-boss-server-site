f7e8e9b3e4a806ecf4c19315b99f784570072f85
shahadat500441 <smd643925@gmail.com>
2025-09-30 14:13:24 +0600

    cart collection added

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT | 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req,res)=>{
+    res.send("bisto is setting")
+})
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mp7na2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.mp7na2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+ const menuCollection = client.db("bistroServer").collection("menu");
+ const reviewsCollection = client.db("bistroServer").collection("reviews");
+ const cartCollection = client.db("bistroServer").collection("carts");
+
+app.get("/menu", async(req,res)=>{
+    const result = await menuCollection.find().toArray();
+    res.send(result)
+})
+app.get("/reviews", async(req,res)=>{
+    const result = await menuCollection.find().toArray();
+    res.send(result)
+})
+
+//cart collection
+app.post("/carts", async(req,res)=>{
+  const cartItem = req.body;
+  const result =await cartCollection.insertOne(cartItem);
+  res.send(result)
+})
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, ()=>{
+    console.log(`bistro boss is running on port : ${port}`)
+})
