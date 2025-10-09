9de3730565e98716e301bf4c474b67c1a7145de4
shahadat500441 <smd643925@gmail.com>
2025-10-09 07:52:21 +0600

    google authentication added and google user quine done

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT | 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("bisto is setting");
+});
 
-app.get("/", (req,res)=>{
-    res.send("bisto is setting")
-})
-
-
-
-
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mp7na2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.mp7na2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -27,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,45 +31,58 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
- const menuCollection = client.db("bistroServer").collection("menu");
- const reviewsCollection = client.db("bistroServer").collection("reviews");
- const cartCollection = client.db("bistroServer").collection("carts");
+    const userCollection = client.db("bistroServer").collection("users");
+    const menuCollection = client.db("bistroServer").collection("menu");
+    const reviewsCollection = client.db("bistroServer").collection("reviews");
+    const cartCollection = client.db("bistroServer").collection("carts");
 
-app.get("/menu", async(req,res)=>{
-    const result = await menuCollection.find().toArray();
-    res.send(result)
-})
-app.get("/reviews", async(req,res)=>{
-    const result = await menuCollection.find().toArray();
-    res.send(result)
-})
+    //user related api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-//cart collection
+    //menu related api
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
 
-app.get("/carts", async(req,res)=>{
-  const email = req.query.email;
-  const query = {email:email}
-  const result = await cartCollection.find(query).toArray();
-  res.send(result)
-})
-app.post("/carts", async(req,res)=>{
+    //cart collection
 
-  const cartItem = req.body;
-  const result =await cartCollection.insertOne(cartItem);
-  res.send(result)
-})
-app.delete("/carts/:id",async(req,res)=>{
-  const id= req.params.id;
-  const query = {_id: new ObjectId(id)}
-    const result = await cartCollection.deleteOne(query);
-    res.send(result)
-  
-})
-
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -81,7 +90,6 @@ app.delete("/carts/:id",async(req,res)=>{
 }
 run().catch(console.dir);
 
-
-app.listen(port, ()=>{
-    console.log(`bistro boss is running on port : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`bistro boss is running on port : ${port}`);
+});
